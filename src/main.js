5f3b18fcb54fcdfd207704594e753df46760d84e
Isak Jonsson <isaksj@stud.ntnu.no>
2025-02-17 21:36:21 +0100

    Fullført grunnleggende layout og funksjonalitet for StartView

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { BiCaretDownFill } from "oh-vue-icons/icons";
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { BiCaretDownFill, BiCaretUpFill } from 'oh-vue-icons/icons'
 
-addIcons(BiCaretDownFill);
+addIcons(BiCaretDownFill, BiCaretUpFill)
 
 const app = createApp(App)
 
-app.component("v-icon", OhVueIcon);
+app.component('v-icon', OhVueIcon)
 app.use(createPinia())
 app.use(router)
 
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,6 +1,17 @@
 <script setup>
 import NavBar from '@/components/NavBarLoggedIn.vue'
 import '@/assets/main.css'
+import { ref } from 'vue'
+
+const numOfTasks = ref('30')
+const species = ref('Hele pensum')
+const quizMode = ref('Artsbestemmelse og normlistestatus')
+const answerMode = ref('Søk og valg')
+const settings = ref(false)
+
+const toggleSettings = () => {
+  settings.value = !settings.value
+}
 </script>
 
 <template>
@@ -11,35 +22,51 @@ import '@/assets/main.css'
     <div class="main">
       <div class="content-box" id="left">
         <div class="wrapper">
-          <h3>Antall oppgaver: {{ 30 }}</h3>
-          <h3>Arter: {{ 'Hele pensum' }}</h3>
-          <h3>Quizmodus: {{ 'Standard' }}</h3>
-          <h3>Svarmodus: {{ 'Standard' }}</h3>
+          <h3>Antall oppgaver: {{ numOfTasks }}</h3>
+          <h3>Arter: {{ species }}</h3>
+          <h3>Quizmodus: {{ quizMode }}</h3>
+          <h3>Svarmodus: {{ answerMode }}</h3>
           <button id="startquiz">Start quiz</button>
-          <div id="avansert-wrapper">
-            <v-icon name="bi-caret-down-fill" />
+          <div id="avansert-wrapper" @click="toggleSettings">
+            <v-icon name="bi-caret-down-fill" v-if="!settings" />
+            <v-icon name="bi-caret-up-fill" v-if="settings" />
             <h3>Avanserte innstillinger</h3>
           </div>
-          <div id="innstillinger-wrapper">
+          <div id="innstillinger-wrapper" v-if="settings">
             <label for="nr-of-tasks">Antall oppgaver:</label>
             <div id="radio-wrapper">
-              <input class="radio" type="radio" id="ten" name="nr-of-tasks" />
+              <input type="radio" id="ten" name="nr-of-tasks" v-model="numOfTasks" value="10" />
               <label for="ten">10</label>
-              <input type="radio" id="twenty" name="nr-of-tasks" />
+              <input type="radio" id="twenty" name="nr-of-tasks" v-model="numOfTasks" value="20" />
               <label for="twenty">20</label>
-              <input type="radio" id="thirty" name="nr-of-tasks" />
+              <input type="radio" id="thirty" name="nr-of-tasks" v-model="numOfTasks" value="30" />
               <label for="thirty">30</label>
-              <input type="radio" id="fourty" name="nr-of-tasks" />
+              <input type="radio" id="fourty" name="nr-of-tasks" v-model="numOfTasks" value="40" />
               <label for="fourty">40</label>
-              <input type="radio" id="fifty" name="nr-of-tasks" />
+              <input type="radio" id="fifty" name="nr-of-tasks" v-model="numOfTasks" value="50" />
               <label for="fifty">50</label>
             </div>
-            <label for="species">Sopparter</label>
-            <select name="species" id="species">
-              <option value="volvo">Hele pensum</option>
-              <option value="saab">Spiselige</option>
-              <option value="opel">Matsopp</option>
-              <option value="opel">Giftige</option>
+            <label for="species">Sopparter:</label>
+            <select name="species" id="species" v-model="species">
+              <option value="Hele pensum">Hele pensum</option>
+              <option value="Spiselige">Spiselige</option>
+              <option value="Matsopp">Matsopp</option>
+              <option value="Giftige">Giftige</option>
+            </select>
+            <label for="quiz-mode">Quizmodus:</label>
+            <select name="quiz-mode" id="quiz-mode" v-model="quizMode">
+              <option value="Artsbestemmelse og normlistestatus">
+                Artsbestemmelse og normlistestatus
+              </option>
+              <option selected="Kun artsbestemmelse">Kun artsbestemmelse</option>
+              <option value="Kun normlistestatus">Kun normlistestatus</option>
+              <option value="Forgiftningsforløp">Forgiftningsforløp</option>
+            </select>
+            <label for="answer-mode">Svarmodus:</label>
+            <select name="answer-mode" id="answer-mode" v-model="answerMode">
+              <option selected="Søk og valg">Søk og valg</option>
+              <option value="Flervalg (4 alternativer)">Flervalg (4 alternativer)</option>
+              <option value="Fritekst">Fritekst</option>
             </select>
           </div>
         </div>
@@ -98,7 +125,7 @@ h3 {
   background-color: #9c914f;
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   font-weight: normal;
-  width: 125%;
+  width: 100%;
   font-size: 25px;
   padding: 10px 0px;
 }
@@ -106,6 +133,10 @@ h3 {
 #avansert-wrapper {
   display: flex;
   flex-direction: row;
+}
+
+#avansert-wrapper:hover {
+  cursor: pointer;
 }
 
 svg {
@@ -128,9 +159,18 @@ svg {
 
 input[type='radio'] {
   margin: 3px 3px 3px 20px;
+  accent-color: black;
 }
 
 label {
   margin: 5px;
+}
+
+select {
+  width: 100%;
+  font-size: 15px;
+  border-radius: 5px;
+  border: none;
+  height: 25px;
 }
 </style>
